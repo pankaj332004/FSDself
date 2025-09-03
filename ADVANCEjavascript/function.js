2724b0008a8240c6585b0b4e8cf0ddd3a57d3f84
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2025-09-04 01:06:30 +0530

    learning javascript function concept

---
+++ ADVANCEjavascript/function.js
@@ -0,0 +1,124 @@
+// function is used to reduce complexity of the program
+// function hello(){
+//     console.log("hello");
+// }
+// hello();
+
+// function printName(){
+//     console.log("ABES Engineering College");
+//     console.log("pankaj kumar rajbhar");
+// }
+// printName();
+
+// function print1to5(){
+//     for(let i=0;i<5;i++){
+//         console.log(i+1);
+//     }
+// }
+// print1to5();
+
+// function isAdult(){
+//     let age = 25;
+//     if(age >= 18){
+//         console.log("adult");
+//     }
+//     else{
+//         console.log("Not adult");
+//     }
+// }
+// isAdult();
+
+// function printPoem(){
+//     console.log("Twinkle twinkle little star");
+//     console.log("how i wonder what you are?");
+// }
+// printPoem();
+
+// function diceRoll(){
+//     console.log(Math.floor(Math.random()*6)+1);
+// }
+// diceRoll();
+// diceRoll();
+// diceRoll();
+// diceRoll();
+// diceRoll();
+// diceRoll();
+// diceRoll();
+// diceRoll();
+// diceRoll();
+// diceRoll();
+
+// function with argument (value pass to the function)
+// order is very compulsory
+// function printName(name){
+//     console.log(name);
+// }
+// printName("Pankaj");
+// printName("Abhii");
+// printName("Agrim ");
+
+// function printInfo(name,age){
+//     console.log(name,age);
+// }
+// printInfo("Pankaj , 22");
+// printInfo("Abhii,20");
+// printInfo("Agrim ,21");
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(10,15);
+// sum(14,58);
+// sum(85,95);
+
+// avg
+// function calAVG(a,b,c){
+//     let avg = (a+b+c)/3;
+//     console.log(Math.floor(avg));
+// }
+// calAVG(2,3,5);
+// calAVG(4,9,10);
+
+//multiplication 
+//  function printTable(n){
+//     for(let i=n;i<=n*10;i+=n){
+//         console.log(i);
+//     }
+//     for(let i=1;i<=10;i++){
+//         console.log(n,"x",i,"=",n*i);
+//     }
+//  }
+//  printTable(5);
+//  printTable(10);
+
+// return keyword
+// function sum(a,b){
+//     console.log("Hello");
+//     console.log("Pankaj");
+//     return a+b;
+//     console.log("nahi chalega");
+// }
+// let s = sum(sum(5,5),20);
+// console.log(s);
+
+// sum 1ton
+// function getsum(n){
+//     let sum = 0;
+//     for(let i=1;i<=n;i++){
+//         sum += i;
+//     }
+//     return sum;
+// }
+// console.log(getsum(1000));
+
+// concatenate
+// let str =["hi","hello","bye","!"];
+// function concat(str){
+//     let result;
+//     for(let i=0;i<str.length;i++){
+//         result += str[i];
+//     }
+//     return result;
+// }
+// console.log(str);
+// console.log(concat(str));
