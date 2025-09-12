3958b462d7c27001c67cb9a5dba56e9c15d50988
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2025-09-12 23:13:10 +0530

    learn about the promises callstack Async and await function

--- ADVANCEjavascript/callStackAsync.js
+++ ADVANCEjavascript/callStackAsync.js
@@ -229,3 +229,71 @@
 //         });
 //     });
 // });
+
+// Async Keyword 
+// async and await
+
+// async function greet(){
+//     return "hello!";
+// }
+
+// greet()
+// .then((res)=>{
+//     console.log("promise was resolved");
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log("Promise was rejected");
+//     console.log(err);
+// })
+
+// await keyword pauses the execution of its surrounding async until the promise is settled (resolved Or rejected)
+
+// function getNum(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             let num = Math.floor(Math.random()*10)+1;
+//             console.log(num);
+//             resolve();
+//         },1000);
+//     })
+// }
+// async function demo() {
+//     await getNum();
+//     await getNum();
+//     await getNum();
+//     await getNum();
+//     getNum();
+// }
+
+
+let h1 = document.querySelector("h1");
+function changeColor(color,delay){
+    return new Promise((resolve , reject)=>{
+        setTimeout(()=>{
+            let num = Math.floor(Math.random()*5)+1;
+            if(num > 3){
+                reject("Promise rejected");
+            }
+        h1.style.color = color;
+        resolve("color change sucessfully");
+    }   ,delay);
+    });
+}
+async function demo() {
+    try{
+        await changeColor("red",1000)
+    await changeColor("blue",1000);
+    await changeColor("green",1000);
+    await changeColor("aqua",1000);
+    await changeColor("purple",1000);
+    }
+    catch(err){
+        console.log("error caught");
+        console.log(err);
+    }
+
+    let a =5;
+    console.log(a);
+    console.log("new number",a+3);
+}
