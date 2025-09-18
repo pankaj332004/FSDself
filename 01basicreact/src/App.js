1b7e4a1cb389584646ff9ecbd96ca68c3ddc1d32
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2025-09-18 06:12:11 +0530

    learning react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>chai aur react | Pankaj Kumar Rajbhar "I have learn about the basic react app and how to create my first react app"</h1>
+  );
+}
+
+export default App;
