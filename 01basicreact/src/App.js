98b03ebd0e2b70cfd4c967628569a554e0337034
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2025-09-18 19:27:03 +0530

    created a file in the src folder and render in app file

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,11 @@
+import Chai from './chai.js'
 
 function App() {
   return (
-    <h1>chai aur react | Pankaj Kumar Rajbhar "I have learn about the basic react app and how to create my first react app"</h1>
+    // <h1>chai aur react | Pankaj Kumar Rajbhar "I have learn about the basic react app and how to create my first react app"</h1>
+    <>
+     <Chai />
+    </>
   );
 }
 
