b40234f4a1da97b4de3bd23b286c2612a2941822
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2025-09-18 20:54:43 +0530

    created my own react library and jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+export function MyApp(){
+  return(
+    <div>
+      <h1>custom app !!</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props:{
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit goolge'
+// }
+
+// const anotherElement = (
+//   <a href='https://google.com' target='_blank'>Visit Google</a>
+// )
+
+const anotheruser = "chai aur react"
+
+const reactElement =React.createElement(
+  'a',
+  {href: 'https://google.com',target:"_blank"},
+  'click me to visit google ',
+  anotheruser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+   reactElement
+  
 )
