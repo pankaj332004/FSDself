5308743dccce4e3dbd52ff7526d62508f7185e53
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2025-11-27 23:09:29 +0530

    In Backend today i have learn about EJS

---
+++ Backend/EJSDIR/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const path =require("path");
+
+const port = 8080;
+
+app.use(express.static(path.join(__dirname,"public/css")));
+app.use(express.static(path.join(__dirname,"public/js")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"/views"));
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
+
+app.get("/hello",(req,res)=>{
+    res.send("Hello");
+});
+
+app.get("/rolldice",(req,res)=>{
+    let diceval = Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{diceval});
+});
+
+app.get("/ig/:username",(req,res)=>{
+    let {username} = req.params;
+    const instadata =  require("./data.json");
+    const data = instadata[username];
+    console.log(data)
+    res.render("instagram.ejs",{data});
+})
+
+app.listen(port , ()=>{
+    console.log(`app is listening on the port ${port}`);
+});
